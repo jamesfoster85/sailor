6175ac8d6353ffaa53a99b0c686955796af8c35b
James Foster <jamesfoster85@gmail.com>
2015-09-12 21:42:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sailor::Application.config.secret_key_base = 'ec38518470c5a0a270e43b4db320325192ea2c2b06c4d1b2ed6dee0fc3799d637636bc1eca43eacfa9f29e0ae6e98b624a9465a92e2f303f55e0c702c7e73e4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sailor::Application.config.session_store :cookie_store, key: '_sailor_session'
