8d99fe3c4ca30f7a14f56a49808b94ac5ed46ae8
James Foster <jamesfoster85@gmail.com>
2015-09-23 12:18:46 -0400

    Accepts new boats and displays on index page

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,9 +1,20 @@
 class BoatsController < ApplicationController
 	def index
-		@boat = Boat.order("RANDOM()").first
+		@boats = Boat.all
 	end
 
 	def new
 		@boat = Boat.new
+	end
+
+	def create
+		Boat.create(boat_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def boat_params
+		params.require(:boat).permit(:kind, :name)
 	end
 end
